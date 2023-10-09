49a0f01c490772e8ff8f4077632d181f4179fa48
layrasfc <layra.cfernandes@gmail.com>
2023-10-09 10:30:00 -0300

    js

---
+++ scripts/main.js
@@ -0,0 +1,147 @@
+const catalogPrincipal = [
+    {
+        cod: 1,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 2,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p3/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 3,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 4,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 5,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg", 
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 6,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 7,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 8,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 9,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 10,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 11,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    },
+    {
+        cod: 12,
+        name: "Caixa de montar peças",
+        price: 49.90,
+        descricao: "A Caixa de Encaixar Formas é um brinquedo sensorial projetado para crianças em fase pré-escolar que oferece uma experiência educativa para o desenvolvimento cognitivo dos pequenos.",
+        imgPrincipal: "./images/produts/p1/principal.jpg",
+        imgExtra: "./images/produts/p1/extra.jpg",
+        imgInfos: "./images/produts/p1/infos.jpg",
+        imgModelos: "./images/produts/p1/modelos.jpg"
+    }  
+]
+
+const gridProdutos = document.querySelector("#options-container")
+
+for (let index = 0; index < catalogPrincipal.length; index++) {
+    const element = catalogPrincipal[index];
+    const produto = `<div class="option">
+    <div class="op-content">
+        <div class="op-img">
+            <img src="${element.imgPrincipal}" class="options-img">
+        </div>
+
+        <div class="op-info">
+            <div class="op-text">
+                <p class="name">${element.name}</p>
+                <p class="price">R$${element.price}</p>
+            </div>
+                <button type="button" class="buy-buttom"><a href="productPage.html"><img src="images/carrinho.svg" class="options-button"></a></button>
+        </div>
+    </div>
+</div>`
+    gridProdutos.innerHTML += produto
+}
+
+
+    
